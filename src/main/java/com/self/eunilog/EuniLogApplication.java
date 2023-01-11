6649098c9b877586cf7f754902b358c50b8fc522
EuniLog <eunisss98@gmail.com>
2023-01-11 22:30:43 +0900

    EuniLog 프로젝트 최초 커밋
    [이슈사항]
     . EuniLog 프로젝트 최초 커밋
    [수정사항]
     . EuniLog 프로젝트 최초 커밋
    [확인사항]
     . 없음

---
+++ src/main/java/com/self/eunilog/EuniLogApplication.java
@@ -0,0 +1,13 @@
+package com.self.eunilog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EuniLogApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EuniLogApplication.class, args);
+    }
+
+}
